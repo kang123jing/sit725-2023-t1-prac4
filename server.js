215bbe0c01bc30dae13200d1867b3d300e2ab786
adhishanand9 <adhishanand9@gmail.com>
2023-08-08 18:23:28 +0530

    Add Files for 4.2P

--- server.js
+++ server.js
@@ -1,11 +1,64 @@
 var express = require("express")
+const { MongoClient, ServerApiVersion } = require('mongodb');
 var app = express()
+const uri = "mongodb+srv://admin:admin@cluster0.oylxykq.mongodb.net/?retryWrites=true&w=majority";
 var port = process.env.port || 3000;
+let collection;
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(port, () => {
-    console.log("App listening to: " + port)
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function runDBConnection() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        collection = client.db().collection('Cat');
+        console.log(collection);
+    } catch (ex) {
+        console.error(ex);
+    }
+}
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+app.get('/api/cats', (req, res) => {
+    getAllCats((err, result) => {
+        if (!err) {
+            res.json({ statusCode: 200, data: result, message: 'get all cats success' });
+        }
+    });
+});
+
+app.post('/api/cat', (req, res) => {
+    let cat = req.body;
+    postCat(cat, (err, result) => {
+        if (!err) {
+            res.json({ statusCode: 201, data: result, message: 'success' });
+        }
+    });
+});
+
+function postCat(cat, callback) {
+    collection.insertOne(cat, callback);
+}
+
+function getAllCats(callback) {
+    collection.find({}).toArray(callback);
+}
+
+app.listen(3000, () => {
+    console.log('express server started');
+    runDBConnection();
+}); 
